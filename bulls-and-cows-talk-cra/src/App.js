4ac00ce16f8192236409c577fb878f11947bfba0
wontae chang <cwt629@naver.com>
2022-12-23 11:52:12 +0900

    feat: create react app and adjust title component

---
+++ bulls-and-cows-talk-cra/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <Title />
+    </div>
+  )
+}
+
+function Title() {
+  return (
+    <div id="title">
+      <h1>숫자야구톡</h1>
+      <h3>made by 장원태</h3>
+    </div>
+  )
+}
+
+export default App;
