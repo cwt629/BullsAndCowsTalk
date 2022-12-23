af7b776bf7a68a1f6b884359a702cd95259d1acb
wontae chang <cwt629@naver.com>
2022-12-23 22:28:02 +0900

    feat: show number inputs on InputShow component

--- bulls-and-cows-talk-cra/src/InputShow.js
+++ bulls-and-cows-talk-cra/src/InputShow.js
@@ -5,13 +5,42 @@ export default function InputShow({ input }) {
         <div id="inputshow">
             <h2>나의 입력</h2>
             <div>
-                <span>1</span>
-                <span>2</span>
-                <span>3</span>
+                {getNumbersToShow(input)}
             </div>
             <p>{getErrorMessage(input)}</p>
         </div>
     )
+}
+
+/**
+ * 숫자가 들어갈 3칸에 출력할 요소들을 받아오는 함수
+ * @param {string} input 
+ * @returns {string[]} 3칸에 각각 들어갈 요소들
+ */
+function getShowboard(input) {
+    let showboard = ["", "", ""]; // 기본적으로 비어있음
+
+    // 올바른 입력인 경우에만 보여준다
+    if (checkIntermediateInput(input) === "none") {
+        for (let i = 0; i < input.length; i++) {
+            showboard[i] = input.charAt(i);
+        }
+    }
+
+    return showboard;
+}
+
+/**
+ * 숫자가 들어갈 3칸에 대한 html 요소를 받아오는 함수
+ * @param {string} input 
+ * @returns 각 요소들을 span으로 감싼 배열
+ */
+function getNumbersToShow(input) {
+    const showboard = getShowboard(input);
+
+    return showboard.map((num) => (
+        <span>{num}</span>
+    ))
 }
 
 /**
